fd92e9ba1c01b43be707f7f3fe4951f5016e0da2
LTH0303 <tianhong018@gmail.com>
2025-12-24 08:46:01 +0800

    update

--- lib/models/AIService/ai_service.dart
+++ lib/models/AIService/ai_service.dart
@@ -9,7 +9,7 @@ class AIService {
 
   AIService() {
     // Ideally use environment variables for keys
-    const apiKey = "AIzaSyB8Ufbx3qf4wTHQifc0gp2mBvguAC9LEj0";
+    const apiKey = "-";
     _model = GenerativeModel(
       model: 'gemini-2.5-flash',
       apiKey: apiKey,
